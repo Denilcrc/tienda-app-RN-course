f955bc8879b08153f118c297f24069b7d7be5b5a
Denilson Cardenas <cardenasguillend@gmail.com>
2025-11-13 22:14:39 -0600

    fuentes anadidas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from 'react-native';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -12,11 +16,30 @@ export const unstable_settings = {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  //? Use `useFonts` only if you can't use the config plugin.
+  const [loaded, error] = useFonts({
+    KanitRegular: require('../assets/fonts/Kanit-Regular.ttf'),
+    KanitThin: require('../assets/fonts/Kanit-Thin.ttf'),
+    KanitBold: require('../assets/fonts/Kanit-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack
+        screenOptions={{ headerShown: false }}
+      >
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} /> */}
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
